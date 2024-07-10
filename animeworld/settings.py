81748f725266384b3ff67986fa4b8801be26b3ee
Surya Pratap <sahusurya1580@gmail.com>
2024-07-10 13:43:20 +0530

    django authentication

--- animeworld/settings.py
+++ animeworld/settings.py
@@ -148,3 +148,25 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
+
+#SMTP Configuration
+
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'suryapratapsahu2017@gmail.com'
+# EMAIL_HOST_PASSWORD = 'Surya@Sahu@2017'
+# EMAIL_USE_TLS = True
+
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'suryapratapsahu2017@gmail.com'
+EMAIL_HOST_PASSWORD = 'zyns luco rhxi yulo'
